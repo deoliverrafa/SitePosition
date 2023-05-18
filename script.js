9e65d34cdfcb3456bf7515b80bb87cd01a1c7db5
deoliverrafa <127500319+deoliverrafa@users.noreply.github.com>
2023-05-18 08:54:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const fechar = document.querySelector('.fa-solid.fa-xmark')
 const modal = document.querySelector('.modal.active')
 const modal_content = document.querySelector('.modal-content.active')
 const btn_reservar = document.querySelector('.reservar')
+const btn_hover = document.querySelector('.fa-solid.fa-bars')
+const display_hover = document.querySelector('.display-hover')
 
 
 fechar.onclick = function (){
@@ -12,4 +14,8 @@ fechar.onclick = function (){
 btn_reservar.onclick= function (){
     modal.classList.remove('active')
     modal_content.classList.remove('active')
+}
+
+btn_hover.onclick= function(){
+    display_hover.classList.toggle('active')
 }

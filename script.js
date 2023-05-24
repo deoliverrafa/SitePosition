e1e7b412d5187b5bcfef5af2d721a3c6c8a49b27
deoliverrafa <127500319+deoliverrafa@users.noreply.github.com>
2023-05-24 17:05:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,40 @@ const modal_content = document.querySelector('.modal-content.active')
 const btn_reservar = document.querySelector('.reservar')
 const btn_hover = document.querySelector('.fa-solid.fa-bars')
 const display_hover = document.querySelector('.display-hover')
+const user_icon = document.querySelector('.fa-solid.fa-user')
+const passwrd_icon = document.querySelector('.fa-solid.fa-lock')
+const name_input = document.querySelector('.name-input')
+const passwrd_input = document.querySelector('.passwrd-input')
+const modal_login = document.querySelector('.modal-login')
+const login_btn = document.querySelector('#login-btn')
+const login_btn_modal = document.querySelector('.login-btn-modal')
+const login_btn_hover = document.querySelector('.display-hover #login-btn')
+const registre = document.querySelector('.registre')
+
+name_input.onclick = function(){
+    user_icon.classList.add('active')
+}
+
+login_btn.onclick = function(){
+    modal_login.classList.add('active')
+}
+
+login_btn_hover.onclick = function(){
+    modal_login.classList.add('active')
+}
+
+login_btn_modal.onclick = function(){
+    modal_login.classList.remove('active')
+    alert("Login Efetuado")
+}
+
+registre.onclick = function(){
+    modal_login.classList.remove('active')
+}
+
+passwrd_input.onfocus = function(){
+    passwrd_icon.classList.add('active')
+}
 
 
 fechar.onclick = function (){
@@ -19,3 +53,4 @@ btn_reservar.onclick= function (){
 btn_hover.onclick= function(){
     display_hover.classList.toggle('active')
 }
+

6bdce17c19af7308bc3573ca482ecc7d6ed89190
deoliverrafa <127500319+deoliverrafa@users.noreply.github.com>
2023-05-25 09:32:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,6 +13,38 @@ const login_btn = document.querySelector('#login-btn')
 const login_btn_modal = document.querySelector('.login-btn-modal')
 const login_btn_hover = document.querySelector('.display-hover #login-btn')
 const registre = document.querySelector('.registre')
+const modal_register = document.querySelector('.modal-register')
+const input_register = document.querySelector('.modal-register .input-register .name-input')
+const input_register_passwrd = document.querySelector('.modal-register .register .box-register .passwrd-input')
+const go_out = document.querySelector('.logo')
+const user_icon_register = document.querySelector('.modal-register .register .fa-solid.fa-user')
+const padlock_icon_register = document.querySelector('.modal-register .register .fa-solid.fa-lock')
+const btn_registrar_modal = document.querySelector('.registrar-btn-modal')
+
+go_out.onclick = function(){
+    modal_content.classList.remove('active')
+}
+
+input_register_passwrd.onclick = function(){
+    padlock_icon_register.classList.add('active')
+}
+
+input_register_passwrd.onfocus = function(){
+    padlock_icon_register.classList.add('active')
+}
+
+btn_registrar_modal.onclick = function(){
+    modal_register.classList.remove('active')
+    alert('Registro Realizado com Sucesso!!!')
+}
+
+input_register.onclick = function(){
+    user_icon_register.classList.add('active')
+}
+
+input_register.onfocus = function(){
+    user_icon_register.classList.add('active')
+}
 
 name_input.onclick = function(){
     user_icon.classList.add('active')
@@ -33,6 +65,7 @@ login_btn_modal.onclick = function(){
 
 registre.onclick = function(){
     modal_login.classList.remove('active')
+    modal_register.classList.add('active')
 }
 
 passwrd_input.onfocus = function(){
